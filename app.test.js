f8e5b9f1771b1158042763f453b096faa6dbf699
Rishiparasa21 <rishiparasa21@gmail.com>
2024-02-08 14:43:02 -0500

    Modified test file

--- app.test.js
+++ app.test.js
@@ -1,13 +1,29 @@
-const request = require("supertest");
-// we also need our app for the correct routes!
-const app = require("./app");
+const request = require('supertest');
+const app = require('./app');
 jest.setTimeout(10000);
 
-describe("GET / ", () => {
-  test("It should respond with an array of cities", async () => {
-    await new Promise((r) => setTimeout(r, 5000));
-    const response = await request(app).get("/cities");
-    expect(response.body).toEqual(["Orlando", "Winter Park", "Oviedo", "Maitland"]);
+describe('GET /joke', () => {
+  test('It should respond with a random joke and punchline', async () => {
+    const response = await request(app).get('/joke');
+    expect(response.body).toHaveProperty('setup');
+    expect(response.body).toHaveProperty('punchline');
     expect(response.statusCode).toBe(200);
   });
 });
+
+describe('GET /joke1', () => {
+  test('It should respond with joke1 setup', async () => {
+    const response = await request(app).get('/joke1');
+    expect(response.body).toEqual({ setup: 'Joke 1 Setup' });
+    expect(response.statusCode).toBe(200);
+  });
+});
+
+describe('GET /punchline1', () => {
+  test('It should respond with joke1 setup and punchline', async () => {
+    const response = await request(app).get('/punchline1');
+    expect(response.body).toEqual({ setup: 'Joke 1 Setup', punchline: 'Joke 1 Punchline' });
+    expect(response.statusCode).toBe(200);
+  });
+});
+
